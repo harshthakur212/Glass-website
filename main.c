e75e72b82b8a8573b4fa7f7f40f0517fe93571a1
harshthakur212 <thakurharsh2122@gmail.com>
2023-05-25 00:43:35 +0530

    completed the website

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int side;
+    printf("Enter your fckn number: ");
+    scanf("%d", &side);
+    printf("The area of your given side is: ", side * side);
+    return 0;
+}
