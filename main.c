e1adb8c5d290c82ccc1903425458acd5f96c1c6d
harshthakur212 <thakurharsh2122@gmail.com>
2023-06-05 17:08:37 +0530

    Final commit

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int side;
-    printf("Enter your fckn number: ");
-    scanf("%d", &side);
-    printf("The area of your given side is: ", side * side);
-    return 0;
-}
